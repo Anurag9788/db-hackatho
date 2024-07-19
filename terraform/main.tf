2972da2c0c6781b122c086a16ed83b25bd416ac2
anurag <anuraganand2020.outlook.com@db-hackathon.com>
2024-07-19 08:03:14 +0530

    removed files

--- terraform/main.tf
+++
@@ -1,7 +0,0 @@
-resource "google_storage_bucket" "my_github_bucket" {
-  name                        = "hack-team-code-breakers_tfc_bucket"
-  location                    = "EU"
-  force_destroy               = true
-  public_access_prevention    = "enforced"
-  uniform_bucket_level_access = true
-}
